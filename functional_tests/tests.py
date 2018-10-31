cd80d5df56b03859825cd897d3753edb5267366d
David Filat <david.filat@eurasiaprecept.org>
2018-11-01 00:26:20 +0200

    Functional test uses LiveServerTestCase now; got rid of time.sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,26 @@
 import time
-import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
+MAX_WAIT = 10
 
-class NewVisitorTest(unittest.TestCase):
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(row_text, [row.text for row in rows])
+
+class NewVisitorTest(LiveServerTestCase):
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(5)
 
     def setUp(self):
         self.browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
@@ -19,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrive_it_later(self):
         # Browser opens the main page
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # Page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -37,7 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Check if items are displayed on the page
-        self.check_for_row_in_list_table("1. Buy some stuff")
+        self.wait_for_row_in_list_table("1. Buy some stuff")
 
         # Add a second item "Buy some other stuff"
         inputbox = self.browser.find_element_by_id("id_new_item")
@@ -46,8 +57,4 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Buy some other stuff")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table("2. Buy some other stuff")
-
-
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
+        self.wait_for_row_in_list_table("2. Buy some other stuff")
