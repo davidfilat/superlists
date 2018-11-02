2eeb8f0f4e7298f3b3bf889fec1b0b574f3b7e82
David Filat <david.filat@eurasiaprecept.org>
2018-11-02 20:39:39 +0200

    Added tests for multiple user lists support

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
 MAX_WAIT = 10
+GECKODRIVER = "/usr/local/bin/geckodriver"
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -23,12 +24,12 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(5)
 
     def setUp(self):
-        self.browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
+        self.browser = webdriver.Firefox(executable_path=GECKODRIVER)
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Browser opens the main page
         self.browser.get(self.live_server_url)
 
@@ -45,10 +46,6 @@ class NewVisitorTest(LiveServerTestCase):
         # User inputs to-do "By some stuff"
         inputbox.send_keys("Buy some stuff")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        # Check if items are displayed on the page
-        self.wait_for_row_in_list_table("1. Buy some stuff")
 
         # Add a second item "Buy some other stuff"
         inputbox = self.browser.find_element_by_id("id_new_item")
@@ -56,5 +53,40 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys("Buy some other stuff")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # Check if items are displayed on the page
+
         self.wait_for_row_in_list_table("2. Buy some other stuff")
+        self.wait_for_row_in_list_table("1. Buy some stuff")
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox(executable_path=GECKODRIVER)
+        # Francis visits the home page.  There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy some stuff", page_text)
+        self.assertNotIn("some other stuff", page_text)
+
+        # Francis starts a new list by entering a new item. He is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1. Buy milk")
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy some stuff", page_text)
+        self.assertIn("Buy milk", page_text)
+
+        # Satisfied, they both go back to sleep
