cd084d75602abcff883ad222f18f2e1f24eec38b
David Filat <david.filat@eurasiaprecept.org>
2019-01-09 14:42:39 +0200

    Create Page objects for list pages, use in sharing FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -114,8 +114,8 @@ class FunctionalTest(StaticLiveServerTestCase):
             session_key = create_session_on_server(email)
         else:
             session_key = create_pre_authenticated_session(email)
-        ## to set a cookie we need to first visit the domain.
-        ## 404 pages load the quickest!
+        # to set a cookie we need to first visit the domain.
+        # 404 pages load the quickest!
         self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(
             dict(name=settings.SESSION_COOKIE_NAME, value=session_key, path="/")
